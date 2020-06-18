19e8069a084124c9495009d661dcdaec80d27cd2
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-19 00:57:47 +0530

    Modified, First Name

--- src/test/java/com/uservalidatortesting/UserValidatorTest.java
+++ src/test/java/com/uservalidatortesting/UserValidatorTest.java
@@ -7,7 +7,14 @@ public class UserValidatorTest {
     @Test
     public void giveFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result= validator.validateFirstName("Nikhil");
-        Assert.assertEquals(true,result);
+        boolean result = validator.validateFirstName("Nikhil");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void giveFirstName_WhenSpecialChar_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Nikh@il");
+        Assert.assertEquals(true, result);
     }
 }
