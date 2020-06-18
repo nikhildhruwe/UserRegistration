b08b68d44a69f4094928feb0d3272517128c3794
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-19 03:17:48 +0530

    Added, Phone Number test cases

--- src/main/java/com/uservalidatortesting/UserValidator.java
+++ src/main/java/com/uservalidatortesting/UserValidator.java
@@ -1,5 +1,6 @@
 package com.uservalidatortesting;
 
+import java.security.PrivateKey;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +10,7 @@ public class UserValidator {
     private static final String LAST_NAME_PATTERN="^[A-Z][a-z]{2,}$";
     private static final String EMAIL_PATTERN="^[a-zA-Z0-9]+([.+-_]?[a-zA-Z0-9]+)*" +
                                                 "@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+    private static final String MOBILE_PATTERN="[0-9]{2}[ |-]?[0-9]{10}$";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern=Pattern.compile(FIRST_NAME_PATTERN);
@@ -27,4 +29,12 @@ public class UserValidator {
         Matcher matcher=pattern.matcher(email2Test);
         return matcher.matches();
     }
+
+    public boolean validatePhoneNumber(String phone) {
+
+        Pattern pattern= Pattern.compile(MOBILE_PATTERN);
+        Matcher matcher=pattern.matcher(phone);
+        return matcher.matches();
+    }
+
 }
--- src/test/java/com/uservalidatortesting/UserValidatorTest.java
+++ src/test/java/com/uservalidatortesting/UserValidatorTest.java
@@ -33,4 +33,30 @@ public class UserValidatorTest {
         Assert.assertEquals(false, result);
     }
 
+    @Test
+    public void givePhoneNumber_WhenProperWithCode_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validatePhoneNumber("90 9876543210");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givePhoneNumber_WhenProperWithDashSymbolAfterCode_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validatePhoneNumber("90-9876543210");
+        Assert.assertEquals(true, result);
+    }
+    @Test
+    public void givePhoneNumber_When10DigitsNotProivded_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validatePhoneNumber("98765432");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givePhoneNumber_WhenAlphabet_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validatePhoneNumber("a987654321");
+        Assert.assertEquals(false, result);
+    }
 }
